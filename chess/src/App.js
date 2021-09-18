23723d94ba391a144b0014d48edda0105b7c5cf3
chisommuorah <muorahmarypeace@gmail.com>
2021-09-18 14:52:32 +0100

    Create App.js

---
+++ chess/src/App.js
@@ -0,0 +1,67 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import HomePage from "./Pages/Homepage.js/Homepage";
+import GameScreenWithComments from "./Pages/GameScreen2/GameScreen2";
+import MainGame from "./Pages/GameScreen/MainGame.js";
+import Modal from "./components/Modals/ModalPage/Modal";
+import InviteModal from "./components/Modals/InviteModal/InviteModal";
+import TestModal from "./components/Modals/AcceptDeclineModal/Test";
+
+import Games from "./components/Games/Games";
+import GameScreenWithoutComments from "./Pages/GameScreen1/GameScreen1";
+import Rules from "./Pages/Rules/Rules";
+import Acceptchallenge from "./components/Modals/Accept_chalengeModal/acceptchalenge";
+
+function App() {
+  return (
+    <div className="App">
+      <Router basename="/chess">
+        <Switch>
+          <Route exact path="/" render={() => <HomePage />} />
+          <Route exact path="/game" render={() => <MainGame />} />
+          <Route exact path="/game/games" render={() => <Games />} />
+          <Route
+            exact
+            path="/game_nocomments"
+            render={() => <GameScreenWithoutComments />}
+          />
+          <Route
+            path="/game_nocomments/:id"
+            render={() => <GameScreenWithoutComments />}
+          />
+          <Route
+            path="/game_nocomments/:id"
+            render={() => <GameScreenWithoutComments />}
+          />
+          <Route
+            exact
+            path="/game_comments"
+            render={() => <GameScreenWithComments />}
+          />
+          <Route exact path="/modalpage" render={() => <Modal />} />
+          <Route exact path="/test-accept-modal" component={TestModal} />
+          <Route exact path="/inviteplayer">
+            <InviteModal />
+          </Route>
+
+          <Route exact path="/acceptchallenge">
+            <Acceptchallenge />
+          </Route>
+
+          <Route
+            exact
+            path="/Accept_chalengeModal"
+            render={() => <Acceptchalengemodal />}
+          />
+
+          <Route exact path="/rules">
+            <Rules />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
